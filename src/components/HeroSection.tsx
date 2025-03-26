f7300097ca5ccba0bd2a1d4d13c57571b1cdfe4a
Darkmindy <stefania.deliso1@gmail.com>
2025-03-26 14:38:21 +0100

    add components

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import styles from './HeroSection.module.css';
+import heroBg from '../assets/images/hero-image.png';
 import FormattedText from '../components/utilities/FormattedText';
 
 const HeroSection: React.FC = () => {
     return (
         <section className={styles.hero}>
-            <div className={styles.leftContent}>
+            <div className={styles.left}>
                 <h1 className={styles.title}>Soulfarm</h1>
                 <h2 className={styles.subtitle}>Agricoltura Familiare</h2>
                 <FormattedText className={styles.description}>
                     L'azienda è certificata biologica ed è composta da orto, frutteto e pascolo, colture cerealicole, vigneto e medicaio.
                 </FormattedText>
             </div>
-            <div className={styles.rightContent}>
-                <div className={styles.heroImage}></div>
-            </div>
+            <div className={styles.right} style={{ backgroundImage: `url(${heroBg})` }}></div>
         </section>
     );
 };
